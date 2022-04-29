9caca1e9d23a05091802018c787a7296f560c686
Bartholomeas <weivan321@gmail.com>
2022-04-29 21:38:58 +0200

    before gulfpile correction

--- src/js/main.ts
+++ src/js/main.ts
@@ -5,12 +5,11 @@
 // console.log('To jest tpyescript');
 
 const burgerBtn: HTMLButtonElement = document.querySelector('.nav__burger--btn');
+const navList: HTMLUListElement = document.querySelector('.nav__list');
 
-function coreFunctions() {
-	handleNavbar();
-}
 function handleNavbar() {
 	console.log('ebebe');
+	navList.classList.toggle('active');
 }
 
-document.addEventListener('DOMContentLoaded', coreFunctions);
+burgerBtn.addEventListener('click', handleNavbar);
